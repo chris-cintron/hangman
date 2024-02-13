fd61ff63bf550988b0972d11be58657867d82ee5
chris-cintron <128305230+chris-cintron@users.noreply.github.com>
2024-02-13 12:42:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-from replit import clear
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
@@ -21,7 +20,7 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    clear()
+    #clear()
     
     if guess in display:
         print(f"You've already guessed {guess}")
